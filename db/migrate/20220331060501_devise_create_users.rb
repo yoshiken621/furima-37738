475ea22690a3017abaa36b610724d7a369346325
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-03-31 15:05:35 +0900

    deviseの再導入（インストールし直し）

--- db/migrate/20220331060501_devise_create_users.rb
+++ db/migrate/20220331060501_devise_create_users.rb
@@ -4,20 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-<<<<<<< Updated upstream
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-=======
-      t.string :nickname,           null: false
-      t.string :email,              null: false, unique: true, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.string :family_name,        null: false
-      t.string :first_name,         null: false
-      t.string :family_name_kana,   null: false
-      t.string :first_name_kana,    null: false
-      t.date   :birthday,           null: false
->>>>>>> Stashed changes
-
 
       ## Recoverable
       t.string   :reset_password_token

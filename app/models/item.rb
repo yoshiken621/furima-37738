0ac23137ec3823aa8dd7057d41b5b8d947b6b6fd
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-04-01 18:30:07 +0900

    アソシエーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchase
+
+  validates :item_name, presence: true
+  validates :item_detail, presence: true
+  validates :item_category_id, presence: true
+  validates :item_status_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :item_price, presence: true
+  validates :user, presence: true, foreign_key: true
+  validates :prefecture_id, presence: true
 end

822b004c76560205b73bda33963b33467ae2b86a
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-04-04 13:55:08 +0900

    ルートとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,15 @@
 class ItemsController < ApplicationController
-  
+ 
+  def new
+    @items = Item.new
+  end
+
+  def create
+    @items = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_detail, :item_category_id, :item_status_id, :delivery_fee_id, :delivery_day_id, :item_price, :user, :prefecture_id).merge(user_id: current_user.id)
+  end
 end

d89bd0c557f5a57da2e4a8f9f5e65e24544b9785
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-04-04 17:40:12 +0900

    アクティブハッシュ・ビューファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_uset!, only:[:new, :create]
  
   def new
     @items = Item.new
@@ -6,10 +7,15 @@ class ItemsController < ApplicationController
 
   def create
     @items = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:item_name, :item_detail, :item_category_id, :item_status_id, :delivery_fee_id, :delivery_day_id, :item_price, :user, :prefecture_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :item_detail, :item_category_id, :item_status_id, :delivery_fee_id, :delivery_day_id, :item_price, :prefecture_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :delivery_fee
+  belongs_to :delivery_day
+  belongs_to :prefecture
   belongs_to :user
   has_one :purchase
+  
 
   validates :item_name, presence: true
   validates :item_detail, presence: true
-  validates :item_category_id, presence: true
-  validates :item_status_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :delivery_day_id, presence: true
-  validates :item_price, presence: true
+  validates :item_price, presence: true, 
   validates :user, presence: true, foreign_key: true
-  validates :prefecture_id, presence: true
+  validates :item_category_id, :item_status_id, :delivery_fee_id, :delivery_day_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end

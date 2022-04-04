e26333e4959188d07714ac1e0abecb89a7392c3f
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-04-04 17:53:39 +0900

    画像投稿機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:item_name, :item_detail, :item_category_id, :item_status_id, :delivery_fee_id, :delivery_day_id, :item_price, :prefecture_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_detail, :item_category_id, :item_status_id, :delivery_fee_id, :delivery_day_id, :item_price, :prefecture_id).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
   
 
   validates :item_name, presence: true

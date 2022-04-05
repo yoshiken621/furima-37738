2cfd1a1724c45bc46f17fd78f992a68707196fab
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-04-05 18:33:19 +0900

    商品表示のためのルーティング、コントローラー、ビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     @items = Item.order('created_at DESC')
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params

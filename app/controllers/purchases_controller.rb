e55e5ea5f08683e4bce4aa23b59ce7b9e576a501
yoshiken621 <100831953+yoshiken621@users.noreply.github.com>
2022-04-08 22:09:12 +0900

    パーチェイス、アドレスコントローラー作成、編集

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,39 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @purchase_address = PurchaseAddress.new
+    @item = Item.find(params[:item_id])
+    if @item.purchase.present?
+      redirect_to root_path
+    elsif @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end 
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    if @purchase_address.valid?
+      pay_info
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city_town, :address, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  end 
+
+  def pay_info
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.item_price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
